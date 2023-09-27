fc2f22954f25220e19728f2d68314e4951f91ae7
yman0784 <117918439+yman0784@users.noreply.github.com>
2023-09-27 20:16:53 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setcount] = useState(0);
+
+  const onClickCountUp = () => {
+    setcount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+    </div>
+  );
+}
